33c6913c6869f1b635dc902f78697332022fcf41
m.levvy <m.levvy@localhost>
2011-06-02 19:20:18 +0200

    Inicjalizacja Jacobiego.

---
+++ src/iofile.c
@@ -0,0 +1,86 @@
+#include "headers/iofile.h"
+
+int readFile(char* filename, matrix_t* A, vector_t* B){
+	int n,i,j;
+	FILE* file = fopen(filename,"r");
+	if(file == NULL){
+		return -2;
+	}
+
+	if(fscanf(file,"%d\n",&n) !=1){
+		return -3;
+	}
+
+	A->n = n;
+	A->a = malloc(n*n*sizeof(double));
+	if(A->a == NULL){
+		return -4;
+	}
+
+	B->n = n;
+	B->b = malloc(n*sizeof(double));
+	if(B->b == NULL){
+		return -4;
+	}
+
+	for(i = 0; i< n; ++i){
+		for(j = 0; j < n; ++j){
+			if(fscanf(file,"%lf",&(A->a[i*n+j])) != 1){
+				/*free mem*/
+				return -3;
+			}
+		}
+	}
+
+	for(i = 0; i < n ; ++i ){
+		if(fscanf(file,"%lf",&(B->b[i])) != 1){
+			/*free mem*/
+			return -3;
+		}
+	}
+
+	fclose(file);
+	return 0;
+}
+
+void printError(int error){
+	printf("Program natrafił na błąd numer %d:\n",error);
+	switch (error){
+		case -1:
+			printf("Złe wywołanie programu, powinno być: mpirun -np 5 ./GaussJacobiDiff plik_wejsciowy [precyzja]");
+			break;
+		case -2:
+			printf("Nie można otworzyć pliku wejściowego\n");
+			break;
+		case -3:
+			printf("Zła składnia pliku wejściowego\n");
+			break;
+		case -4:
+			printf("Nie można zaalokować pamięci\n");
+			break;
+		case -55:
+			printf("Układ równań ma nieskończenie wiele rozwiązań lub jest sprzeczny\n");
+			break;
+	}
+}
+
+void printResults(char* header,vector_t X,struct timeval start, struct timeval end){
+	int i = 0;
+	int sec,size;
+	long milisec;
+	printf("Zakończono obliczenia metodą %s.\nOtrzymano następujący wektor:\n",header);
+	for(; i < X.n; i++){
+		printf("X[%d] = %g\n",i,X.b[i]);
+	}
+	sec = end.tv_sec-start.tv_sec;
+	if(end.tv_usec < start.tv_usec){
+		sec--;
+	}
+	milisec = sec*1000000 + (end.tv_usec-start.tv_usec);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	printf("Metoda %s - czas pracy: %ld mikrosekund dla rzędu układu %d i %d procesów\n",header, milisec,X.n,size);
+}
+
+void printTimeDiff(struct timeval times[4]){
+	/*TODO wypisać info o różnicy czasów*/
+}
---
+++ src/main.c
@@ -0,0 +1,81 @@
+#include "headers/iofile.h"
+#include "headers/def.h"
+#include "headers/util.h"
+#include <string.h>
+#include "headers/gaussJordan.h"
+
+int main(int argc, char** argv){
+	int size,rank,error,equalsSize;
+	char* inputFile = NULL;
+	double precision;
+	matrix_t A;
+	vector_t B, X;
+	int *beginIndexes, *endIndexes;
+	struct timeval start,end;
+	struct timezone timezone;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+	if(rank == 0){
+		error = getParams(argc,argv,inputFile,&precision);
+		if(error != 0){
+			printError(error);
+			MPI_Abort(MPI_COMM_WORLD,-1);
+			return -1;
+		}
+		error = readFile(argv[1],&A,&B);
+		if(error != 0){
+			printError(error);
+			MPI_Abort(MPI_COMM_WORLD,-2);
+			return -2;
+		}
+		error = calculateIndexes(A.n,&beginIndexes,&endIndexes,&equalsSize);
+		if(error != 0){
+			printError(error);
+			MPI_Abort(MPI_COMM_WORLD,-3);
+			return -3;
+		}
+	}
+
+	/*
+	 * Gauss-Jordan
+	 */
+	if(rank==0){
+		gettimeofday(&start,&timezone);
+	}
+	calculateGauss(A,B,&X,beginIndexes,endIndexes);
+	if(rank==0){
+		gettimeofday(&end,&timezone);
+	}
+	if(rank == 0){
+		printResults("Gauss-Jordan",X,start,end);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	/*
+	 * Jacobi
+	 */
+	if(rank==0){
+			gettimeofday(&start,&timezone);
+		}
+	calculateJacobi(A,B,&X,beginIndexes,endIndexes);
+	if(rank==0){
+		gettimeofday(&end,&timezone);
+	}
+	if(rank == 0){
+		printResults("Jacobi",X,start,end);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+
+
+	if(rank == 0){
+		freeMatrix(&A);
+		freeVector(&B);
+		free(beginIndexes);
+		free(endIndexes);
+	}
+	MPI_Finalize();
+	return 0;
+}
