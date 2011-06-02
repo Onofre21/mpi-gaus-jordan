33a787e67aec0a467c93d42c06dabba3aefaab38
alien11689 <alien11689@localhost>
2011-06-02 09:14:16 +0200

    gauss sekwencyjny jeszcze nie sko?czony

--- src/gaussJordan.c
+++ src/gaussJordan.c
@@ -157,3 +157,51 @@ int calculateGauss(matrix_t A,vector_t B, vector_t* X, int* beginIndexes, int* e
 	free(pivotRow);
 	return 0;
 }
+
+int calculateGaussJordanSequence(matrix_t A,vector_t B, vector_t* X){
+	int i,j,k,which;
+	double tmp;
+	//double *pivotRow = (double*) malloc((A.n+1) * sizeof(double));
+	double *markedRows = (int*) malloc(A.n * sizeof(int));
+	double *columnChecked = (int*) malloc(A.n * sizeof(int));
+	for(i =0; i < A.n; i++){
+		markedRows[i] = 0;
+	}
+	// i to kolumna, j to wiersz
+	for(i = 0; i < A.n; i++){
+		tmp = 0.0;
+		for(j = 0; j < A.n; j++){
+			if(fabs(A.a[j*A.n+i]) > tmp){
+				tmp = fabs(A.a[j*A.n+i]);
+				which = j;
+			}
+		}
+		if(tmp<EPSILON){
+			free(markedRows);
+			free(columnChecked);
+			return -5;
+		}
+		markedRows[which] = 1;
+		columnChecked[which] = i;
+		for(j = 0; j < A.n; j++){
+			if(!(markedRows[j] && columnChecked[j] == i)){
+				tmp = A.a[j*A.n+i] / A.a[which*A.n+i];
+				A.a[j*A.n+i] = 0;
+				for( k = i+1; k < A.n; k++){
+					A.a[j*A.n+k] -= A.a[which*A.n+k]*tmp;
+				}
+				B.b[j] -= B.b[which]*tmp;
+			}
+		}
+	}
+	X->n = A.n;
+	X->b = malloc(A.n*sizeof(double));
+	for(i = 0; i < X->n; i++){
+
+	}
+
+	//free(pivotRow);
+	free(markedRows);
+	free(columnChecked);
+	return 0;
+}
