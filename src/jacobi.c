cfc4e1fd69eac6eeb33f1ff87641779b62ad17c4
m.levvy <m.levvy@localhost>
2011-06-02 19:23:27 +0200

    Czyszczenie starych wynik?w.

--- src/jacobi.c
+++ src/jacobi.c
@@ -7,18 +7,24 @@
 
 #include"headers/jacobi.h"
 
-int calculateJacobi(matrix_t A,vector_t B, vector_t* X, int* beginIndexes, int* endIndexes){
+int cleanResult(vector_t *X) {
+	int i;
+	for (i = 0; i < X->n; i++) {
+		X->b[i] = 0;
+	}
+}
+
+int calculateJacobi(matrix_t A, vector_t B, vector_t* X, int* beginIndexes, int* endIndexes) {
 	int rank, procSize;
 	int i, j;
 	MPI_Status status;
-	MPI_Comm_size(MPI_COMM_WORLD,&procSize);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &procSize);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if(rank == 0){
-		for(i = 0; i<X->n;i++){
-			X->b[i] = 0;
-		}
+	if (rank == 0) {
+		cleanResult(X);
 	}
+
 
 	return 0;
 }
