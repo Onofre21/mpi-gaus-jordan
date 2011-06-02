d02dc183e8e309e53a62fc5975e55e0f6f6e6fe9
m.levvy <m.levvy@localhost>
2011-06-02 20:25:28 +0200

    Macierz D odwrocone L U licza sie dobrze

--- src/jacobi.c
+++ src/jacobi.c
@@ -30,8 +30,8 @@ int allocMemory(matrix_t *M, vector_t *N, matrix_t *D, matrix_t *L, matrix_t *U 
 		return -4;
 	}
 
-	U->a = malloc(n*n*sizeof(double));
-	if(U->a == NULL){
+	L->a = malloc(n*n*sizeof(double));
+	if(L->a == NULL){
 		return -4;
 	}
 
@@ -39,28 +39,26 @@ int allocMemory(matrix_t *M, vector_t *N, matrix_t *D, matrix_t *L, matrix_t *U 
 	if(U->a == NULL){
 		return -4;
 	}
+	return 0;
 }
 
 void freeMemory(matrix_t *M, vector_t *N, matrix_t *D, matrix_t *L, matrix_t *U){
 	free(M->a);
-	free(M);
 	free(N->b);
-	free(N);
 	free(D->a);
-	free(D);
 	free(L->a);
-	free(L);
 	free(U->a);
-	free(U);
 }
 
 int calculateRDiagonal(matrix_t *A, matrix_t *D){
 	int i, j;
 	int n;
 	int pos;
+	int size;
 	n = A->n;
+	size = n*n;
 	for(i = 0; i < n ; ++i){
-		for(j = 0; j <n; ++j){
+		for(j = 0; j < n; ++j){
 			pos = i * n + j;
 			if(j == i){
 				D->a[pos] = 1/(A->a[pos]);
@@ -69,8 +67,59 @@ int calculateRDiagonal(matrix_t *A, matrix_t *D){
 			}
 		}
 	}
+//	for(i = 0 ; i < size; ++i){
+//		printf("Diagonala %g \n", D->a[i]);
+//	}
 	return 0;
 }
+
+int calculateL(matrix_t *A, matrix_t *L){
+	int i, j;
+	int n;
+	int pos;
+	int size;
+	n = A->n;
+	size = n*n;
+	for(i = 0; i < n ; ++i){
+		for(j = 0; j < n; ++j){
+			pos = i * n + j;
+			if(j < i){
+				L->a[pos] = A->a[pos];
+			}else{
+				L->a[pos] = 0;
+			}
+		}
+	}
+	//for(i = 0 ; i < size; ++i){
+	//	printf("Lower %g \n", L->a[i]);
+	//}
+	return 0;
+}
+
+
+int calculateU(matrix_t *A, matrix_t *U){
+	int i, j;
+		int n;
+		int pos;
+		int size;
+		n = A->n;
+		size = n*n;
+		for(i = 0; i < n ; ++i){
+			for(j = 0; j < n; ++j){
+				pos = i * n + j;
+				if(j > i){
+					U->a[pos] = A->a[pos];
+				}else{
+					U->a[pos] = 0;
+				}
+			}
+		}
+//    for(i = 0 ; i < size; ++i){
+//	  printf("Lower %g \n", U->a[i]);
+//	}
+	return 0;
+}
+
 
 
 
@@ -88,10 +137,13 @@ int calculateJacobi(matrix_t A, vector_t B, vector_t* X, int* beginIndexes, int*
 
 	if(rank == 0){
 		if(allocMemory(&M,&N,&D,&L,&U, M.n) != 0){
-			freeMemory(&M,&N,&D,&L,&U);
+			freeMemory(&M,&N,&D,&L,&U); //TODO To jest do poprawienia
+			printError(-4);
 			return -1;
 		}
-		//calculateRDiagonal(&A, &D);
+		calculateRDiagonal(&A, &D);
+		calculateL(&A, &L);
+		calculateU(&A, &U);
 		freeMemory(&M,&N,&D,&L,&U);
 	}
 
