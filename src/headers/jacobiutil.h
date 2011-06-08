1323e356f5e595cb603156f566806270b60bedb6
m.levvy <m.levvy@localhost>
2011-06-08 22:07:46 +0200

    Koniec :)

--- src/headers/jacobiutil.h
+++ src/headers/jacobiutil.h
@@ -18,5 +18,7 @@ void calculateU(matrix_t *A, matrix_t *U) ;
 void calculateM(matrix_t *M, matrix_t *D, matrix_t *L, matrix_t *U) ;
 void calculateN(vector_t *N, matrix_t *D, vector_t *B) ;
 int checkMatrix(matrix_t *A);
+int allcMemoryAll(vector_t *XResult, vector_t *XResultOld, int **beginIndexes, int **endIndexes, int rowSize, int rank, int procSize);
+void freeMemoryAll(int **beginIndexes, int **endIndexes, vector_t *XResult, vector_t *XResultOld);
 
 #endif /* JACOBIUTIL_H_ */
